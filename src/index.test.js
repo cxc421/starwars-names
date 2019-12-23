fb7040476be591bafdb1daee643df54417239235
Chris Chuang <chrischuang0421@gmail.com>
2019-12-23 15:59:57 +0800

    chore: add travis config. conventional commit. adn semantic-release
    BREAKING CHANGE: closes #1
    
    fix #1

---
+++ src/index.test.js
@@ -0,0 +1,25 @@
+const expect = require("chai").expect;
+const starWarsNames = require("./index");
+
+describe("starwars-names", () => {
+  describe("all", () => {
+    it("should be an array of string", () => {
+      expect(starWarsNames.all).to.satisfies(isArrayOfString);
+
+      function isArrayOfString(array) {
+        return array.every(item => typeof item === "string");
+      }
+    });
+
+    it("should contain `Like Skywalker`", () => {
+      expect(starWarsNames.all).to.include("Luke Skywalker");
+    });
+  });
+
+  describe("random", () => {
+    it("should return a random item from the starWarsNames.all", () => {
+      const randomItem = starWarsNames.random();
+      expect(starWarsNames.all).to.include(randomItem);
+    });
+  });
+});
